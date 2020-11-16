aefa013ccec3c186b3755e319d6ce137bd1aecd5
NickDev90 <nickie05021990@gmail.com>
2020-11-16 21:14:18 +0100

    bugs fixing

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,24 @@
 document.querySelector('.head_inner nav ul li.switch a').onclick = function (e) {
 	document.querySelector('body').classList.toggle('switchMode');
 	e.preventDefault;
-
-}
+};
 
 document.querySelector('.burger').onclick = function () {
 	this.classList.toggle('active');
 	document.querySelector('.menu').classList.toggle('active');
 	document.querySelector('body').classList.toggle('lock');
 };
+
+document.querySelector('.menu ul').onclick = function (e) {
+	document.querySelector('.menu').classList.remove('active');
+	document.querySelector('.burger').classList.remove('active');
+	document.querySelector('body').classList.remove('lock');
+};
+
+// window.addEventListener("resize", function() {
+//     if (window.matchMedia("(max-width: 767px)").matches) {
+//         document.querySelector('#switch').innerHTML = 'switch';
+//     } 
+// });
+
+// document.querySelector('#switch').innerHTML = 'switch';
