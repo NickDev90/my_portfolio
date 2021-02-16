f4c2c8acc98730c51b1c72c29f1403fba92dc2f4
NickDev90 <nickie05021990@gmail.com>
2021-02-16 22:01:43 +0100

    slick added

--- JS/main.js
+++ JS/main.js
@@ -15,10 +15,58 @@ document.querySelector('.menu ul').onclick = function (e) {
 	document.querySelector('body').classList.remove('lock');
 };
 
-// window.addEventListener("resize", function() {
-//     if (window.matchMedia("(max-width: 767px)").matches) {
-//         document.querySelector('#switch').innerHTML = 'switch';
-//     } 
-// });
 
-// document.querySelector('#switch').innerHTML = 'switch';
+$(document).ready(function () {
+
+	// Slick slider
+	$('.portfolio_items').slick({
+	  infinite: true,
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  accessibility: false,
+	  arrows: true,
+	  dots: false,
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 2,
+	        infinite: true,
+	      }
+	    },
+	    {
+	      breakpoint: 767,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1,
+	        slickRemove: 3
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	      	slidesToShow: 1,
+	        slidesToScroll: 1,
+	        arrows: false,
+	        dots: true
+	      }
+	    }
+  		]
+	});
+
+	//Menu scrolling	
+	$('.menu ul a[href^="#"').click(function () {
+		var target = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top -100
+		}, 300);
+	}) 
+
+	$('.btn__contact').click(function () {
+		var target = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top -100
+		}, 300);
+	}) 
+
+})
